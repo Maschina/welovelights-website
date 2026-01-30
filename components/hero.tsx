171ecb239bb330646d62f86c4766e79a33a1eed0
Robert Hahn <robert.hahn@mailbox.org>
2026-01-30 23:11:40 +0100

    Update hero image and add image zoom

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
-import macbookHeroImage from "@/assets/Macbook_Hero_Image.png";
+import heroImage from "@/assets/Hero-1.png";
 import textLogoDark from "@/assets/text-logo-dark@2x.png";
-import downloadOnTheMacAppStoreBadgeUsUkRgbWht092917 from "@/assets/Download_on_the_Mac_App_Store_Badge_US-UK_RGB_wht_092917.svg";
+import appStoreBadge from "@/assets/Download_on_the_Mac_App_Store_Badge_US-UK_RGB_wht_092917.svg";
 import Link from "next/link";
 import { ScrollIndicator } from "./scroll-indicator";
 import { getLatestVersion } from "@/lib/changelog";
 import { Spotlight } from "@/components/ui/spotlight";
+import Zoom from 'react-medium-image-zoom'
+import "@/app/react-medium-image-zoom.css"
 
 export default function Hero() {
     const latestVersion = getLatestVersion();
@@ -17,18 +19,34 @@ export default function Hero() {
                 fill="white"
             />
             <div className="flex flex-row gap-[38px] w-full items-center justify-center sm:items-start sm:justify-end xl:justify-center overflow-hidden">
-                <Image
-                    src={macbookHeroImage}
-                    alt="We Love Lights Screenshot with open menu from the menu bar and open Studio window"
-                    width={1256}
-                    height={1016}
-                    loading="eager"
-                    className="hidden sm:block relative w-[628px] h-[508px]"
-                    priority
-                />
+                <div className="hidden sm:block w-[628px] shrink-0">
+                    <Zoom
+                        zoomMargin={60}
+                        zoomImg={{
+                            alt: "We Love Lights Screenshot with open menu from the menu bar and open Studio window",
+                            src: heroImage.src,
+                        }} 
+                    >
+                        <Image
+                            src={heroImage}
+                            alt="We Love Lights Screenshot with open menu from the menu bar and open Studio window"
+                            width={628} // original width: 2926px
+                            height={311} // original height: 1788px
+                            loading="eager"
+                            className="relative w-[628px]"
+                            priority
+                        />
+                    </Zoom>
+                </div>
                 <div className="flex flex-col items-center sm:items-start gap-[40px] pr-3 py-[17px] shrink-0">
                     <div className="flex relative">
-                        <Image src={textLogoDark} alt="App Logo" className="w-[280px]" width={219} height={45} loading="eager" priority />
+                        <Image
+                            src={textLogoDark}
+                            alt="App Logo"
+                            className="w-[280px]"
+                            width={219} height={45}
+                            loading="eager" priority
+                        />
                         <p className="sr-only">We Love Lights</p>
                     </div>
                     <h1 className="font-bold text-white text-center sm:text-left text-6xl sm:text-7xl tracking-tight leading-14 sm:leading-18">
@@ -40,7 +58,7 @@ export default function Hero() {
                             <Image
                                 className="relative w-[160px] sm:w-[190px]"
                                 alt="Download on the Mac App Store"
-                                src={downloadOnTheMacAppStoreBadgeUsUkRgbWht092917}
+                                src={appStoreBadge}
                                 width={163.05}
                                 height={41.8}
                             />
@@ -51,12 +69,13 @@ export default function Hero() {
                         <Link href="https://testflight.apple.com/join/HAK6qEjT" className="flex text-muted text-xs hover:underline">Interested in Version 3.7 beta?</Link>
                     </div>
                     <Image
-                        src={macbookHeroImage}
+                        src={heroImage}
                         alt="We Love Lights Screenshot with open menu from the menu bar and open Studio window"
-                        width={628}
-                        height={508}
+                        width={628} // original width: 2926px
+                        height={311} // original height: 1788px
                         loading="eager"
-                        className="block sm:hidden relative w-[350px]"
+                        className="relative block sm:hidden w-[350px]"
+                        priority
                     />
                 </div>
             </div>
